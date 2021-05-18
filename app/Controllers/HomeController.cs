848c7b8cbdfa50ed736e5ae01c484c6027a4041f
Ramaraj Marimuthu <ramaraj.marimuthu@syncfusion.com>
2021-05-18 14:29:39 +0530

    Added a Word Document generation codes.

--- app/Controllers/HomeController.cs
+++ app/Controllers/HomeController.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
@@ -6,6 +6,9 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using app.Models;
+using System.IO;
+using Syncfusion.DocIO.DLS;
+using Syncfusion.DocIO;
 
 namespace app.Controllers
 {
@@ -18,12 +21,50 @@ namespace app.Controllers
             _logger = logger;
         }
 
-        public IActionResult Index()
-        {
-            return View();
-        }
+    public IActionResult Index(string button)
+    {
+      if (button == null)
+        return View();
+      MemoryStream outputStream;
+      try
+      {
+        // Loads document from stream.
+        WordDocument document = new WordDocument();
+        document.EnsureMinimal();
+        document.LastParagraph.AppendText("Document from Openshift");
 
-        public IActionResult Privacy()
+        ////Instantiation of DocIORenderer for Word to PDF conversion
+        //DocIORenderer render = new DocIORenderer();
+        ////Converts Word document into PDF document
+        //PdfDocument pdfDocument = render.ConvertToPDF(document);
+        ////Releases all resources used by the Word document and DocIO Renderer objects
+        //render.Dispose();
+        //document.Dispose();
+        ////Saves the PDF file
+        //outputStream = new MemoryStream();
+        //pdfDocument.Save(outputStream);
+        ////Closes the instance of PDF document object
+        //pdfDocument.Close();
+
+        outputStream = new MemoryStream();
+        document.Save(outputStream, FormatType.Docx);
+        document.Dispose();
+
+        outputStream.Position = 0;
+
+      }
+      catch (Exception e)
+      {
+        ViewBag.Error = "SkiaNativeAssets.NoDependencies   " + e.Message + e.StackTrace.ToString();
+        return View();
+
+      }
+      //return File(outputStream, "application/pdf", "Test.pdf");
+      return File(outputStream, "application/msword", "Test.docx");
+
+    }
+
+    public IActionResult Privacy()
         {
             return View();
         }
