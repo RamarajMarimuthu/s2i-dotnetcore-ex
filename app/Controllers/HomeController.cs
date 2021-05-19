62098dafb6d147218181a9455446952644c67201
Ramaraj Marimuthu <ramaraj.marimuthu@syncfusion.com>
2021-05-19 12:29:09 +0530

    Added a chiller font details in Aspose.Words.

--- app/Controllers/HomeController.cs
+++ app/Controllers/HomeController.cs
@@ -26,8 +26,35 @@ namespace app.Controllers
       if (button == null)
         return View();
       MemoryStream outputStream;
+      MemoryStream outputStream2;
       try
       {
+        // load the file to be converted
+        Document document = new Document();
+
+        DocumentBuilder builder = new DocumentBuilder(document);
+
+        // Specify font formatting
+        Font font = builder.Font;
+        font.Size = 16;
+        font.Bold = true;
+        font.Color = System.Drawing.Color.Blue;
+        font.Name = "Chiller";
+        font.Underline = Underline.Dash;
+
+        // Specify paragraph formatting
+        ParagraphFormat paragraphFormat = builder.ParagraphFormat;
+        paragraphFormat.FirstLineIndent = 8;
+        paragraphFormat.Alignment = ParagraphAlignment.Justify;
+        paragraphFormat.KeepTogether = true;
+
+        builder.Writeln("Aspose paragraph.");
+
+        //Saves the PDF file
+        outputStream = new MemoryStream();
+        // save in different formats
+        document.Save(outputStream, Aspose.Words.SaveFormat.Pdf);
+
         SKBitmap bitmap = new SKBitmap(100, 100, SKImageInfo.PlatformColorType, SKAlphaType.Premul);
         var surface = SKSurface.Create(bitmap.Info);
         SKCanvas canvas = surface.Canvas;
@@ -40,44 +67,17 @@ namespace app.Controllers
         canvas.DrawCircle(50, 50, 25, paint);
         var image = SKImage.FromBitmap(bitmap);
         var data = surface.Snapshot().Encode(SKEncodedImageFormat.Jpeg, 80);
-        outputStream = new MemoryStream();
-        data.SaveTo(outputStream);
-        outputStream.Position = 0;
+        outputStream2 = new MemoryStream();
+        data.SaveTo(outputStream2);
+        outputStream2.Position = 0;
 
         ViewBag.Error = "SkiaSharp loaded properly.";
-        return View();
 
-        //// load the file to be converted
-        //Document document = new Document();
-
-        //DocumentBuilder builder = new DocumentBuilder(document);
-
-        //// Specify font formatting
-        //Font font = builder.Font;
-        //font.Size = 16;
-        //font.Bold = true;
-        //font.Color = System.Drawing.Color.Blue;
-        //font.Name = "Arial";
-        //font.Underline = Underline.Dash;
-
-        //// Specify paragraph formatting
-        //ParagraphFormat paragraphFormat = builder.ParagraphFormat;
-        //paragraphFormat.FirstLineIndent = 8;
-        //paragraphFormat.Alignment = ParagraphAlignment.Justify;
-        //paragraphFormat.KeepTogether = true;
-
-        //builder.Writeln("Aspose paragraph.");
-
-        ////Saves the PDF file
         //outputStream = new MemoryStream();
-        //// save in different formats
-        //document.Save(outputStream, Aspose.Words.SaveFormat.Pdf);
+        //document.Save(outputStream, FormatType.Docx);
+        //document.Dispose();
 
-        ////outputStream = new MemoryStream();
-        ////document.Save(outputStream, FormatType.Docx);
-        ////document.Dispose();
-
-        //outputStream.Position = 0;
+        outputStream.Position = 0;
 
       }
       catch (Exception e)
@@ -86,7 +86,7 @@ namespace app.Controllers
         return View();
 
       }
-      //return File(outputStream, "application/pdf", "Test.pdf");
+      return File(outputStream, "application/pdf", "Test.pdf");
       //return File(outputStream, "application/msword", "Test.docx");
     }
 
