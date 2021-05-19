acfb52806e004deded1132881fdc327aa25e0c0a
Ramaraj Marimuthu <ramaraj.marimuthu@syncfusion.com>
2021-05-19 12:02:16 +0530

    Added a SkiaSharp code changes

--- app/Controllers/HomeController.cs
+++ app/Controllers/HomeController.cs
@@ -8,6 +8,7 @@ using Microsoft.Extensions.Logging;
 using app.Models;
 using System.IO;
 using Aspose.Words;
+using SkiaSharp;
 
 namespace app.Controllers
 {
@@ -27,46 +28,65 @@ namespace app.Controllers
       MemoryStream outputStream;
       try
       {
-        // load the file to be converted
-        Document document = new Document();
-
-        DocumentBuilder builder = new DocumentBuilder(document);
-
-        // Specify font formatting
-        Font font = builder.Font;
-        font.Size = 16;
-        font.Bold = true;
-        font.Color = System.Drawing.Color.Blue;
-        font.Name = "Arial";
-        font.Underline = Underline.Dash;
-
-        // Specify paragraph formatting
-        ParagraphFormat paragraphFormat = builder.ParagraphFormat;
-        paragraphFormat.FirstLineIndent = 8;
-        paragraphFormat.Alignment = ParagraphAlignment.Justify;
-        paragraphFormat.KeepTogether = true;
-
-        builder.Writeln("Aspose paragraph.");
-
-        //Saves the PDF file
+        SKBitmap bitmap = new SKBitmap(100, 100, SKImageInfo.PlatformColorType, SKAlphaType.Premul);
+        var surface = SKSurface.Create(bitmap.Info);
+        SKCanvas canvas = surface.Canvas;
+        canvas.Clear(SKColors.White);
+        var paint = new SKPaint();
+        paint.IsAntialias = true;
+        paint.Color = SKColors.Red;
+        paint.StrokeWidth = 3;
+        string fontName = SKTypeface.Default.FamilyName;
+        canvas.DrawCircle(50, 50, 25, paint);
+        var image = SKImage.FromBitmap(bitmap);
+        var data = surface.Snapshot().Encode(SKEncodedImageFormat.Jpeg, 80);
         outputStream = new MemoryStream();
-        // save in different formats
-        document.Save(outputStream, Aspose.Words.SaveFormat.Pdf);
-
-        //outputStream = new MemoryStream();
-        //document.Save(outputStream, FormatType.Docx);
-        //document.Dispose();
-
+        data.SaveTo(outputStream);
         outputStream.Position = 0;
+
+        ViewBag.Error = "SkiaSharp loaded properly.";
+        return View();
+
+        //// load the file to be converted
+        //Document document = new Document();
+
+        //DocumentBuilder builder = new DocumentBuilder(document);
+
+        //// Specify font formatting
+        //Font font = builder.Font;
+        //font.Size = 16;
+        //font.Bold = true;
+        //font.Color = System.Drawing.Color.Blue;
+        //font.Name = "Arial";
+        //font.Underline = Underline.Dash;
+
+        //// Specify paragraph formatting
+        //ParagraphFormat paragraphFormat = builder.ParagraphFormat;
+        //paragraphFormat.FirstLineIndent = 8;
+        //paragraphFormat.Alignment = ParagraphAlignment.Justify;
+        //paragraphFormat.KeepTogether = true;
+
+        //builder.Writeln("Aspose paragraph.");
+
+        ////Saves the PDF file
+        //outputStream = new MemoryStream();
+        //// save in different formats
+        //document.Save(outputStream, Aspose.Words.SaveFormat.Pdf);
+
+        ////outputStream = new MemoryStream();
+        ////document.Save(outputStream, FormatType.Docx);
+        ////document.Dispose();
+
+        //outputStream.Position = 0;
 
       }
       catch (Exception e)
       {
-        ViewBag.Error = "SkiaNativeAssets.NoDependencies   " + e.Message + e.StackTrace.ToString();
+        ViewBag.Error = e.Message + e.StackTrace.ToString();
         return View();
 
       }
-      return File(outputStream, "application/pdf", "Test.pdf");
+      //return File(outputStream, "application/pdf", "Test.pdf");
       //return File(outputStream, "application/msword", "Test.docx");
     }
 
